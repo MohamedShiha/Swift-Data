7a6fc5834249fa299a0c1e883893d447becf75b3
Mohamed Shiha <mohamedshiha15@gmail.com>
2023-12-11 21:04:24 +0200

    Initial commit

---
+++ Swift-Data/Swift_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_DataApp.swift
+//  Swift-Data
+//
+//  Created by Mohamed Shiha on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

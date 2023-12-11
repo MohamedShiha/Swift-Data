068adb41f163bba9fe7ab1e8bf2279fea9c0a30d
Mohamed Shiha <mohamedshiha15@gmail.com>
2023-12-11 21:36:06 +0200

    Added the env object for the context.

--- Swift-Data/Swift_DataApp.swift
+++ Swift-Data/Swift_DataApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct Swift_DataApp: App {
@@ -13,5 +14,6 @@ struct Swift_DataApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: GroceryItem.self)
     }
 }

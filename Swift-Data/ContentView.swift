8c50dbe4330ef6290c1a929c7795b47e0a965e75
Mohamed Shiha <mohamedshiha15@gmail.com>
2023-12-11 21:35:16 +0200

    Added the UI elements for the playground.

--- Swift-Data/ContentView.swift
+++ Swift-Data/ContentView.swift
@@ -6,16 +6,43 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext) var context
+    @State private var newGroceryItem: String = ""
+    @Query private var groceryItems: Array<GroceryItem>
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                TextField("Add Item", text: $newGroceryItem)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Button("Save") {
+                    guard !newGroceryItem.isEmpty else { return }
+                    let groceryItem = GroceryItem(title: newGroceryItem, subtitle: "Get it asap!", date: .now)
+                    context.insert(groceryItem)
+                    newGroceryItem = ""
+                }
+                List {
+                    ForEach(groceryItems) { item in
+                        Text(item.title)
+                    }
+                    .onDelete { indexSet in
+                        indexSet.forEach { index in
+                            context.delete(groceryItems[index])
+                        }
+                    }
+                }
+                .overlay {
+                    if groceryItems.isEmpty {
+                        Text("No Items")
+                    }
+                }
+            }
+            .navigationTitle("GroceryItems")
         }
-        .padding()
     }
 }
 
